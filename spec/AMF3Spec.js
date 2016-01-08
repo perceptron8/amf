06ea1baf01b38159e0415ec7ee557616eceb5a8e
perceptron8 <perceptron8@gmail.com>
2016-01-08 14:02:02 +0100

    cleanup

--- spec/AMF3Spec.js
+++ spec/AMF3Spec.js
@@ -3,24 +3,8 @@
 var _ = require("underscore");
 
 var AMF3 = require("../lib/AMF3");
-
-var INTEGER_MIN = -Math.pow(2, 28);
-var INTEGER_MAX = +Math.pow(2, 28) - 1;
-
-function pullFrom(array) {
-	return function(length) {
-		return Uint8Array.from(
-			array.splice(0, length)
-		);
-	}
-}
-function pushTo(array) {
-	return function(bytes) {
-		for (var byte of bytes) {
-			array.push(byte);
-		}
-	}
-}
+var Helpers = require("./Helpers");
+var Constants = require("./AMF3/Constants");
 
 describe("AMF3", function() {
 	it("has Marker", function() {
@@ -37,8 +21,8 @@ describe("AMF3", function() {
 	
 	it("deals well with powers of two", function() {
 		var buffer = [];
-		var writer = new AMF3.Writer(pushTo(buffer));
-		var reader = new AMF3.Reader(pullFrom(buffer));
+		var writer = new AMF3.Writer(Helpers.pushTo(buffer));
+		var reader = new AMF3.Reader(Helpers.pullFrom(buffer));
 		for (var i = 0; i < 28; i++) {
 			var v = 1 << i;
 			writer.write(v);
@@ -48,10 +32,10 @@ describe("AMF3", function() {
 	
 	it("deals well with random integers", function() {
 		var buffer = [];
-		var writer = new AMF3.Writer(pushTo(buffer));
-		var reader = new AMF3.Reader(pullFrom(buffer));
+		var writer = new AMF3.Writer(Helpers.pushTo(buffer));
+		var reader = new AMF3.Reader(Helpers.pullFrom(buffer));
 		for (var t = 0; t < 100; t++) {
-			var v = _.random(INTEGER_MIN, INTEGER_MAX);
+			var v = _.random(Constants.INTEGER_MIN, Constants.INTEGER_MAX);
 			writer.write(v);
 			expect(reader.read()).toEqual(v);
 		}
@@ -59,8 +43,8 @@ describe("AMF3", function() {
 	
 	it("deals well with random doubles", function() {
 		var buffer = [];
-		var writer = new AMF3.Writer(pushTo(buffer));
-		var reader = new AMF3.Reader(pullFrom(buffer));
+		var writer = new AMF3.Writer(Helpers.pushTo(buffer));
+		var reader = new AMF3.Reader(Helpers.pullFrom(buffer));
 		var m = 1 << 29;
 		for (var t = 0; t < 100; t++) {
 			var r = Math.random();
--- spec/AMFSpec.js
+++ spec/AMFSpec.js
@@ -1,15 +1,71 @@
 "use strict";
 
 var AMF = require("../lib/AMF");
+var AMF0 = require("../lib/AMF0");
+var AMF3 = require("../lib/AMF3");
+var Helpers = require("./Helpers");
+
+var TextEncoder = require("text-encoding").TextEncoder;
+var TextDecoder = require("text-encoding").TextDecoder;
+var NumberEncoder = require("number-encoding").NumberEncoder;
+var NumberDecoder = require("number-encoding").NumberDecoder;
+
+var utf8encoder = new TextEncoder("utf-8");
+var u16encoder = new NumberEncoder("Uint16");
+var u16encoder = new NumberEncoder("Uint16");
+
+// test data
+
+var header = new AMF.Header("name", "value");
+var message = new AMF.Message("targetUri", "responseUri");
+var packet = new AMF.Packet([header], [message]);
+
+var UNKNOWN_LENGTH = [0xFF, 0xFF, 0xFF, 0xFF];
+
+var chunks = [].concat([
+	// version
+	u16encoder.encode(packet.version),
+	// headers (length)
+	u16encoder.encode(packet.headers.length),
+	// headers (array)
+	u16encoder.encode(header.name.length),
+	utf8encoder.encode(header.name),
+	[header.mustUnderstand ? 1 : 0],
+	UNKNOWN_LENGTH,
+	[AMF0.Marker.STRING],
+	u16encoder.encode(header.value.length),
+	utf8encoder.encode(header.value),
+	// messages (length)
+	u16encoder.encode(packet.messages.length),
+	// messages (array)
+	u16encoder.encode(message.targetUri.length),
+	utf8encoder.encode(message.targetUri),
+	u16encoder.encode(message.responseUri.length),
+	utf8encoder.encode(message.responseUri),
+	UNKNOWN_LENGTH,
+	[AMF0.Marker.AVMPLUS_OBJECT],
+	[AMF3.Marker.UNDEFINED]
+]);
+
+var buffer = [];
+for (var chunk of chunks) {
+	for (var byte of chunk) {
+		buffer.push(byte);
+	}
+}
+
+// expectations
 
 describe("AMF.Packet", function() {
-	it("can write null", function() {
-		var value = {
-			"leet": 1337
-		};
-		var header = new AMF.Header("name", "value");
-		var message = new AMF.Message("targetUri", "responseUri", value);
-		var packet = new AMF.Packet([header], [message]);
-		// expect(packet.encode()).toEqual([]);
+	it("can encode", function() {
+		var encoded = [];
+		var push = Helpers.pushTo(encoded);
+		packet.encode(push);
+		expect(encoded).toEqual(buffer);
+	});
+	it("can decode", function() {
+		var pull = Helpers.pullFrom(buffer);
+		var decoded = AMF.Packet.decode(pull);
+		expect(decoded).toEqual(packet);
 	});
 });
